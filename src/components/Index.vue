d56cc7ff25e0c37a414a7eb477ac24bae6eb157d
TheDoubleRainbow <16mb.com@gmail.com>
2018-01-19 18:24:50 +0200

    Init commit

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+	<Top/>
+</template>
+
+<script>
+	import Top from '@/components/Top'
+
+	export default{
+		name: "index",
+		data () {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang="sass" scoped>
+	
+</style>
---
+++ src/components/Top.vue
@@ -0,0 +1,119 @@
+<template>
+	<header>
+		<div class="columns is-centered">
+			<div class="column recent">
+				Recent files
+			</div>
+		</div>
+		<div class="recent-files columns is-centered">
+			<div v-for="i in a" class="recent-file column is-2">
+				<div class="recent-file-header">
+					<span class="recent-file-type">Image</span>
+					<span class="recent-file-name">An Interesting Picture</span>
+				</div>
+				<div class="recent-file-body">
+				</div>
+				<div class="recent-file-footer">
+					View
+				</div>
+			</div>
+		</div>
+		<nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+			<div class="navbar-brand">
+			</div>
+			<div class="navbar-menu">
+				<a class="navbar-item">
+				  Home
+				</a>
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link">
+						Music
+					</a>
+					<div class="navbar-dropdown is-boxed">
+						<a v-for="i in a" class="navbar-item">
+				  			Type
+						</a>
+					</div>
+				</div>
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link">
+						Video
+					</a>
+					<div class="navbar-dropdown is-boxed">
+						<a v-for="i in a" class="navbar-item">
+				  			Type
+						</a>
+					</div>
+				</div>
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link">
+						Text
+					</a>
+					<div class="navbar-dropdown is-boxed">
+						<a v-for="i in a" class="navbar-item">
+				  			Type
+						</a>
+					</div>
+				</div>
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link">
+						Pictures
+					</a>
+					<div class="navbar-dropdown is-boxed">
+						<a v-for="i in a" class="navbar-item">
+				  			Type
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script>
+	export default{
+		name: "Top",
+		data () {
+			return {
+				a: [,,,,]
+			}
+		}
+	}
+</script>
+
+<style lang="sass" scoped>
+	header
+		background: #424242
+		height: 350px
+		font-size: 12pt
+	.recent
+		color: white
+		text-align: center
+		font-size: 20pt
+		margin: 10px 0px 15px
+	.recent-file
+		background: #F5F9FC
+		border-radius: 3px
+		padding: 0
+		margin-left: 30px
+	.recent-file-body
+		background: #5FA3B8
+		height: 160px
+	.recent-file-type
+		border-radius: 3px 0px 0px 0px
+		background: #1CBBB0
+		display: inline-block
+		padding: 3px 5px 3px 5px
+		color: white
+	.recent-file-footer
+		cursor: pointer
+		text-align: center
+		padding: 2px
+		transition-duration: 0.3s
+	.recent-file-footer:hover
+		color: #2F69B9
+	.navbar
+		top: 25px
+		box-shadow: 0px 0px 6px #979797
+
+</style>
